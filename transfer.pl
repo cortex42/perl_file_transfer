5745aecbd978e60f30c457fef2125a75ce24173a
Ivo <cortex42@users.noreply.github.com>
2018-08-19 22:12:57 +0200

    Update transfer.pl

--- transfer.pl
+++ transfer.pl
@@ -4,7 +4,7 @@ use strict;
 use IO::Socket;
 
 if ($#ARGV + 1 < 2 || $#ARGV + 1 > 3) {
-    print "usage: $0 [-l port | ip port file]\n";
+    print "usage: $0 [-l port [file] | ip port [file]]\n";
     exit;
 }
 
@@ -21,24 +21,49 @@ if ($ARGV[0] eq "-l") { # listen mode
     die "Could not create socket: $!\n" unless $sock;
     # print "listening on port $ARGV[1]\n";
 
-    my $new_sock = $sock->accept();
-    while(<$new_sock>) { print $_; }
-    close($sock);
+    if ($#ARGV + 1 eq 3) {
+        my $new_sock = $sock->accept();
+
+        open (FILE, $ARGV[2]) || die "could not open file $ARGV[2]\n";
+        while (<FILE>) {
+            print $new_sock $_;
+        }
+        close($sock);
+        close(FILE);
+    } else {
+        my $new_sock = $sock->accept();
+        while(<$new_sock>) { print $_; }
+        close($sock);
+    }
 }
 else { # connect mode
-    my $sock = new IO::Socket::INET (
-        PeerAddr => $ARGV[0],
-        PeerPort => $ARGV[1],
-        Proto => 'tcp'
-    );
+        if ($#ARGV + 1 eq 3) {
+        my $sock = new IO::Socket::INET (
+            PeerAddr => $ARGV[0],
+            PeerPort => $ARGV[1],
+            Proto => 'tcp'
+        );
 
-    die "could not create socket: $!\n" unless $sock;
-    # print "connected to $ARGV[0]:$ARGV[1]\n";
+        die "could not create socket: $!\n" unless $sock;
+        # print "connected to $ARGV[0]:$ARGV[1]\n";
 
-    open(FILE, $ARGV[2]) || die "could not open file $ARGV[2]\n";
+        open(FILE, $ARGV[2]) || die "could not open file $ARGV[2]\n";
 
-    while (<FILE>) {
-        print $sock $_;
+        while (<FILE>) {
+            print $sock $_;
+        }
+        close(FILE);
+    } else {
+        my $sock = new IO::Socket::INET (
+            PeerAddr => $ARGV[0],
+            PeerPort => $ARGV[1],
+            Proto => 'tcp'
+        );
+
+        die "could not create socket: $!\n" unless $sock;
+        # print "connected to $ARGV[0]:$ARGV[1]\n";
+
+        while(<$sock>) { print $_; }
+        close($sock);
     }
-    close(FILE);
 }
